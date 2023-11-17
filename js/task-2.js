887c6838e92aae9b728514059abf308a579e706c
KorotinI <142391798+KorotinI@users.noreply.github.com>
2023-11-17 23:13:44 +0100

    4

--- js/task-2.js
+++ js/task-2.js
@@ -4,14 +4,12 @@ function calcAverageCalories(days) {
   if (days.length === 0) {
     return 0;
   }
-
   const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
   return totalCalories / days.length;
-
 }
 
-console.log(
-  calcAverageCalories([
+console.log(calcAverageCalories
+        ([
     { day: "monday", calories: 3010 },
     { day: "tuesday", calories: 3200 },
     { day: "wednesday", calories: 3120 },
@@ -22,8 +20,8 @@ console.log(
   ])
 ); // 3180
 
-console.log(
-  calcAverageCalories([
+console.log(calcAverageCalories
+        ([
     { day: "monday", calories: 2040 },
     { day: "tuesday", calories: 2270 },
     { day: "wednesday", calories: 2420 },
--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const profile = {
+const profile =
+{
   username: "Jacob",
   playTime: 300,
   changeUsername: function (newName) {
@@ -15,9 +16,7 @@ const profile = {
 };
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
 profile.changeUsername("Marco");
 console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
 profile.updatePlayTime(20);
 console.log(profile.getInfo()); // "Marco has 320 active hours!"
